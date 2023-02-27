91be413110470a9c393fcae108b26af2a59a8012
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-27 08:25:40 +0200

    Adds PostCSS and some useful utilities.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{astro,html,jsx,tsx,svelte,vue,js,ts}"],
+	content: [
+		"./public/**/*.html",
+		"./src/**/*.{astro,html,jsx,tsx,svelte,vue,js,ts}"
+	],
 	theme: {
 		extend: {
 			screens: {
-				sm: "400px",
-			},
-		},
+				sm: "400px"
+			}
+		}
 	},
-	plugins: [],
+	plugins: []
 };
