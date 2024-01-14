030c050115cd280fa4db08daa1570dadb5fe2f5d
mrtingalingling <ting.lik.2@gmail.com>
2024-01-13 22:29:49 -0500

    add React CRA

--- astro.config.ts
+++ astro.config.ts
@@ -1,41 +1,35 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 
 // Astro integration imports
-import tailwind from "@astrojs/tailwind"
-import sitemap from "@astrojs/sitemap"
-import compress from "astro-compress"
-import { VitePWA } from "vite-plugin-pwa"
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+import { VitePWA } from "vite-plugin-pwa";
 
 // Helper imports
-import { manifest, seoConfig } from "./utils/seoConfig"
+import { manifest, seoConfig } from "./utils/seoConfig";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-	site: seoConfig.baseURL,
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false,
-				path: "./tailwind.config.js"
-			}
-		}),
-		sitemap(),
-		compress()
-	],
-	vite: {
-		plugins: [
-			VitePWA({
-				registerType: "autoUpdate",
-				manifest,
-				workbox: {
-				  globDirectory: 'dist',
-				  globPatterns: [
-				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-				  ],
-				  // Don't fallback on document based (e.g. `/some-page`) requests
-				  // This removes an errant console.log message from showing up.
-				  navigateFallback: null,
-				},
-			})
-		]
-	}
-})
+  site: seoConfig.baseURL,
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false,
+      path: "./tailwind.config.js"
+    }
+  }), sitemap(), compress(), react()],
+  vite: {
+    plugins: [VitePWA({
+      registerType: "autoUpdate",
+      manifest,
+      workbox: {
+        globDirectory: 'dist',
+        globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null
+      }
+    })]
+  }
+});
